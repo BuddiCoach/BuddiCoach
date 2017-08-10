893493ea68295058b3ad7911437133009fb18530
Petra Vos <petravos70@gmail.com>
2017-08-10 18:34:16 +0200

    Layout homepage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import NavbarHeader from './NavbarHeader';
+import AppName from './AppName';
+import Form from './Form'
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = { message: '' }
-  }
-
   componentDidMount() {
     fetch('/api/message')
-    .then(response => response.json())
-    .then(json => this.setState({ message: json }));
+      .then(response => response.json())
+      .then(json => this.setState({message: json}));
   }
 
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>{this.state.message}</h2>
+        <NavbarHeader/>
+        <div className="container">
+          <AppName/>
+          <Form/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
