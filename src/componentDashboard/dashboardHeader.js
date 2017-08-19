8a0f870cd9c770b55a060f1797958a7b1a98df4a
Petra Vos <petravos70@gmail.com>
2017-08-19 16:21:40 +0200

    made change on dashboardcomponent/css

--- src/componentDashboard/dashboardHeader.js
+++ src/componentDashboard/dashboardHeader.js
@@ -8,17 +8,18 @@ import {Link} from 'react-router-dom';
 class DashboardHeader extends Component {
     render(){
         return(
-         <div className="App-dashboard">
+            <div className="name-app">
+            <div className="logoDashboard">
+            <h1>BuddiCoach</h1><span className="dashboard-logo"></span>
+            </div>
+            <div className="app-dashboard">
           <ul className="nav-dashboard">
             <li ><Link to="/">Name User</Link></li>
             <li><Link to="/">Log Out</Link></li>
           </ul>
-          <div className="app-name">
-           <div className="app-logo">
-           <h1>BuddiCoach</h1><span className="logo"></span>
-           </div>
           </div>
          </div>
+         
         )
     }     
 }
